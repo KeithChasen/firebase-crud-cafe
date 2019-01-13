6e6808da6e7d82831e437f88d7e789b467a4c423
keithchasenm <keithchasen89@gmail.com>
2019-01-13 22:48:24 +0200

    add record to db

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const cafeList = document.querySelector('#cafe-list')
+const form = document.querySelector('#add-cafe-form')
 
 //create element and render cafe
-function renderCafe(doc) {
+renderCafe = doc => {
     let li = document.createElement('li')
     let name = document.createElement('span')
     let city = document.createElement('span')
@@ -15,8 +16,21 @@ function renderCafe(doc) {
     cafeList.appendChild(li)
 }
 
+//getting data
 db.collection('cafes').get().then(response => {
     response.docs.forEach(doc => {
         renderCafe(doc)
     })
 })
+
+//saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    })
+    form.name.value = ''
+    form.city.value = ''
+})
+
